6227698a69e6463a76c928d66e5e4eb78eb32b28
Charlie Thomas <charlie.thomas@attwoodthomas.net>
2017-05-28 16:20:33 +0100

    Add permissions_required decorator

---
+++ internal/decorators.py
@@ -0,0 +1,17 @@
+from functools import wraps
+
+from flask import abort, g
+
+from internal.exceptions import CustomError
+
+
+def permissions_required(permissions):
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            if g.user.has_permissions(permissions):
+                return f(*args, **kwargs)
+
+            raise CustomError(403)
+        return decorated_function
+    return decorator
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'internal-utils',
-    version = '0.0.1',
+    version = '0.0.2',
     description = 'internal utility methods',
     packages = [
         'internal',
